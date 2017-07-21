90f2e118b0c35ca59d43d23bb5d3c7287c4b8cde
ishant <ityagi@infoblox.com>
2017-07-21 15:33:24 +0530

    Fix Lock() method of NetworkViewLock

--- lock.go
+++ lock.go
@@ -13,18 +13,18 @@ const (
 )
 
 type Lock interface {
-	Lock() bool
-	UnLock(force bool) bool
+	Lock() error
+	UnLock(force bool) error
 }
 
-type NetworkViewLocker struct {
-	name          string
-	objMgr        *ObjectManager
+type NetworkViewLock struct {
+	Name          string
+	ObjMgr        *ObjectManager
 	LockEA        string
 	LockTimeoutEA string
 }
 
-func (l *NetworkViewLocker) createLockRequest() *MultiRequest {
+func (l *NetworkViewLock) createLockRequest() *MultiRequest {
 
 	req := NewMultiRequest(
 		[]*RequestBody{
@@ -32,7 +32,7 @@ func (l *NetworkViewLocker) createLockRequest() *MultiRequest {
 				Method: "GET",
 				Object: "networkview",
 				Data: map[string]interface{}{
-					"name":         l.name,
+					"name":         l.Name,
 					"*" + l.LockEA: freeLockVal,
 				},
 				Args: map[string]string{
@@ -49,7 +49,7 @@ func (l *NetworkViewLocker) createLockRequest() *MultiRequest {
 				Data: map[string]interface{}{
 					"extattrs+": map[string]interface{}{
 						l.LockEA: map[string]string{
-							"value": l.objMgr.tenantID,
+							"value": l.ObjMgr.tenantID,
 						},
 						l.LockTimeoutEA: map[string]int32{
 							"value": int32(time.Now().Unix()),
@@ -80,11 +80,11 @@ func (l *NetworkViewLocker) createLockRequest() *MultiRequest {
 	return req
 }
 
-func (l *NetworkViewLocker) createUnlockRequest(force bool) *MultiRequest {
+func (l *NetworkViewLock) createUnlockRequest(force bool) *MultiRequest {
 
-	getData := map[string]interface{}{"name": l.name}
+	getData := map[string]interface{}{"name": l.Name}
 	if !force {
-		getData["*"+l.LockEA] = l.objMgr.tenantID
+		getData["*"+l.LockEA] = l.ObjMgr.tenantID
 	}
 
 	req := NewMultiRequest(
@@ -146,18 +146,18 @@ func (l *NetworkViewLocker) createUnlockRequest(force bool) *MultiRequest {
 	return req
 }
 
-func (l *NetworkViewLocker) Lock() bool {
-	logrus.Debugf("Creating lock on network niew %s\n", l.name)
+func (l *NetworkViewLock) getLock() bool {
+	logrus.Debugf("Creating lock on network niew %s\n", l.Name)
 	req := l.createLockRequest()
-	res, err := l.objMgr.CreateMultiObject(req)
+	res, err := l.ObjMgr.CreateMultiObject(req)
 
 	if err != nil {
-		logrus.Debugf("Failed to create lock on network view %s: %s\n", l.name, err)
+		logrus.Debugf("Failed to create lock on network view %s: %s\n", l.Name, err)
 
 		//Check for Lock Timeout
-		nw, err := l.objMgr.GetNetworkView(l.name)
+		nw, err := l.ObjMgr.GetNetworkView(l.Name)
 		if err != nil {
-			logrus.Debugf("Failed to get the network view object for %s : %s\n", l.name, err)
+			logrus.Debugf("Failed to get the network view object for %s : %s\n", l.Name, err)
 			return false
 		}
 
@@ -167,14 +167,14 @@ func (l *NetworkViewLocker) Lock() bool {
 				//remove the lock forcefully and acquire it
 				l.UnLock(true)
 				// try to get lock again
-				return l.Lock()
+				return l.getLock()
 			}
 		}
 		return false
 	}
 
 	dockerID := res[0]["DOCKER-ID"]
-	if dockerID == l.objMgr.tenantID {
+	if dockerID == l.ObjMgr.tenantID {
 		logrus.Debugln("Got the lock !!!")
 		return true
 	}
@@ -182,63 +182,66 @@ func (l *NetworkViewLocker) Lock() bool {
 	return false
 }
 
-func (l *NetworkViewLocker) UnLock(force bool) bool {
-	// To unlock set the Docker-Plugin-Lock EA of network view to Available and
-	// remove the Docker-Plugin-Lock-Time EA
-	req := l.createUnlockRequest(force)
-	res, err := l.objMgr.CreateMultiObject(req)
-
-	if err != nil {
-		logrus.Debugf("Failed to release lock on Network View %s: %s\n", l.name, err)
-		return false
-	}
-
-	dockerID := res[0]["DOCKER-ID"]
-	if dockerID == freeLockVal {
-		logrus.Debugln("Removed the lock!")
-		return true
-	}
-
-	return false
-}
-
-func GetNVLock(netViewName string, objMgr *ObjectManager, lockEA string, lockTimeoutEA string) (Lock, error) {
+func (l *NetworkViewLock) Lock() (error) {
 
 	// verify if network view exists and has EA for the lock
-	nw, err := objMgr.GetNetworkView(netViewName)
+	nw, err := l.ObjMgr.GetNetworkView(l.Name)
 	if err != nil {
-		msg := fmt.Sprintf("Failed to get the network view object for %s : %s\n", netViewName, err)
+		msg := fmt.Sprintf("Failed to get the network view object for %s : %s\n", l.Name, err)
 		logrus.Debugf(msg)
-		return nil, fmt.Errorf(msg)
+		return fmt.Errorf(msg)
 	}
 
-	if _, ok := nw.Ea[lockEA]; !ok {
-		err = objMgr.UpdateNetworkViewEA(nw.Ref, EA{lockEA: freeLockVal}, nil)
+	if _, ok := nw.Ea[l.LockEA]; !ok {
+		err = l.ObjMgr.UpdateNetworkViewEA(nw.Ref, EA{l.LockEA: freeLockVal}, nil)
 		if err != nil {
-			return nil, fmt.Errorf("Failed to Update Network view with Lock EA")
+			return fmt.Errorf("Failed to Update Network view with Lock EA")
 		}
 	}
 
-	l := &NetworkViewLocker{name: netViewName, objMgr: objMgr, LockEA: lockEA, LockTimeoutEA: lockTimeoutEA}
+
 	retryCount := 0
 	for {
 		// Get lock on the network view
-		lock := l.Lock()
+		lock := l.getLock()
 		if lock == true {
 			// Got the lock.
-			logrus.Debugf("Got the lock on Network View %s\n", netViewName)
-			return l, nil
+			logrus.Debugf("Got the lock on Network View %s\n", l.Name)
+			return nil
 		} else {
 			// Lock is held by some other agent. Wait for some time and retry it again
 			if retryCount >= 10 {
-				return nil, fmt.Errorf("Failed to get Lock on Network View %s", netViewName)
+				return fmt.Errorf("Failed to get Lock on Network View %s", l.Name)
 			}
 
 			retryCount++
-			logrus.Debugf("Lock on Network View %s not free. Retrying again %d out of 10.\n", netViewName, retryCount)
+			logrus.Debugf("Lock on Network View %s not free. Retrying again %d out of 10.\n", l.Name, retryCount)
 			// sleep for random time (between 1 - 10 seconds) to reduce collisions
 			time.Sleep(time.Duration(rand.Intn(9)+1) * time.Second)
 			continue
 		}
 	}
+}
+
+func (l *NetworkViewLock) UnLock(force bool) error {
+	// To unlock set the Docker-Plugin-Lock EA of network view to Available and
+	// remove the Docker-Plugin-Lock-Time EA
+	req := l.createUnlockRequest(force)
+	res, err := l.ObjMgr.CreateMultiObject(req)
+
+	if err != nil {
+		msg := fmt.Sprintf("Failed to release lock from Network View %s: %s\n", l.Name, err)
+		logrus.Errorf(msg)
+		return fmt.Errorf(msg)
+	}
+
+	dockerID := res[0]["DOCKER-ID"]
+	if dockerID == freeLockVal {
+		logrus.Debugln("Removed the lock!")
+		return nil
+	}
+
+	msg := fmt.Sprintf("Failed to release lock from Network View %s\n", l.Name)
+	logrus.Errorf(msg)
+	return fmt.Errorf(msg)
 }
