2575656a5c9f26705cc57a22c56eb50288566579
Yury Tsarev <yury.tsarev@absa.africa>
2020-03-05 15:14:29 +0100

    CRUD management for TXT record

--- object_manager.go
+++ object_manager.go
@@ -562,6 +562,66 @@ func (objMgr *ObjectManager) DeleteCNAMERecord(ref string) (string, error) {
 	return objMgr.connector.DeleteObject(ref)
 }
 
+func (objMgr *ObjectManager) CreateTXTRecord(recordname string, text string, dnsview string) (*RecordTXT, error) {
+
+	recordTXT := NewRecordTXT(RecordTXT{
+		View: dnsview,
+		Name: recordname,
+		Text: text,
+	})
+
+	ref, err := objMgr.connector.CreateObject(recordTXT)
+	recordTXT.Ref = ref
+	return recordTXT, err
+}
+
+func (objMgr *ObjectManager) GetTXTRecordByRef(ref string) (*RecordTXT, error) {
+	recordTXT := NewRecordTXT(RecordTXT{})
+	err := objMgr.connector.GetObject(recordTXT, ref, &recordTXT)
+	return recordTXT, err
+}
+
+func (objMgr *ObjectManager) GetTXTRecord(name string) (*RecordTXT, error) {
+	if name == "" {
+		return nil, fmt.Errorf("name can not be empty")
+	}
+	var res *RecordTXT
+
+	recordTXT := NewRecordTXT(RecordTXT{Name: name})
+
+	err := objMgr.connector.GetObject(recordTXT, "", &res)
+
+	if err != nil || res == nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (objMgr *ObjectManager) UpdateTXTRecord(recordname string, text string) (*RecordTXT, error) {
+	var res []RecordTXT
+
+	recordTXT := NewRecordTXT(RecordTXT{Name: recordname})
+
+	err := objMgr.connector.GetObject(recordTXT, "", &res)
+
+	res[0].Text = text
+
+	res[0].Zone = "" //  set the Zone value to "" as its a non writable field
+
+	_, err = objMgr.connector.UpdateObject(&res[0], res[0].Ref)
+
+	if err != nil || res == nil || len(res) == 0 {
+		return nil, err
+	}
+
+	return &res[0], nil
+}
+
+func (objMgr *ObjectManager) DeleteTXTRecord(ref string) (string, error) {
+	return objMgr.connector.DeleteObject(ref)
+}
+
 func (objMgr *ObjectManager) CreatePTRRecord(netview string, dnsview string, recordname string, cidr string, ipAddr string, ea EA) (*RecordPTR, error) {
 
 	eas := objMgr.extendEA(ea)
