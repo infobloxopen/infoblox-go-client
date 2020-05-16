98df1e173a184a7a3079480cde16d171baea13c7
Anagha K H <43703238+Anaghakh@users.noreply.github.com>
2020-05-16 19:37:41 +0530

    Add files via upload

---
+++ record_aaaa.go
@@ -0,0 +1,130 @@
+package ibclient
+
+import (
+	"fmt"
+	_ "time"
+)
+
+type RecordAAAAOperations interface {
+	CreateAAAARecord(recA4 RecordAAAA) (*RecordAAAA, error)
+	GetAAAARecord(recA4 RecordAAAA) (*[]RecordAAAA, error)
+	DeleteAAAARecord(recA4 RecordAAAA) (string, error)
+	UpdateAAAARecord(recA4 RecordAAAA) (*RecordAAAA, error)
+}
+
+type RecordAAAA struct {
+	IBBase   `json:"-"`
+	Ref              string `json:"_ref,omitempty"`
+	Ipv6Addr         string `json:"ipv6addr,omitempty"`
+	Name             string `json:"name,omitempty"`
+	View             string `json:"view,omitempty"`
+	Zone             string `json:"zone,omitempty"`
+	Ea               EA     `json:"extattrs,omitempty"`
+	NetView          string `json:"omitempty"`
+	Cidr             string `json:"omitempty"`
+	AddEA            EA     `json:"omitempty"`
+	RemoveEA         EA     `json:"omitempty"`
+	Creation_time    int    `json:"creation_time,omitempty"`
+	Comment          string `json:"comment,omitempty"`
+	Creator          string  `json:"creator,omitempty"`
+	Ddns_protected   bool    `json:"ddns_protected,omitempty"`
+	Dns_name         string  `json:"dns_name,omitempty"`
+	Forbid_reclamation bool  `json:"forbid_reclamation,omitempty"`
+	Reclaimable      bool     `json:"reclaimable,omitempty"`
+ 	Ttl              uint     `json:"ttl,omitempty"`
+	Use_ttl          bool     `json:"use_ttl,omitempty"`
+}
+
+func NewRecordAAAA(ra RecordAAAA) *RecordAAAA {
+	res := ra
+	res.objectType = "record:aaaa"
+	res.returnFields = []string{ "ipv6addr", "name", "view", "zone","extattrs","comment","creation_time",
+				     "creator","ddns_protected","dns_name","cloud_info","forbid_reclamation","last_queried",
+				      "reclaimable","ttl","use_ttl","aws_rte53_record_info","ddns_principal","disable","discovered_data","ms_ad_user_data"}
+	return &res
+}
+
+// CreateAAAARecord takes Name, Ipv6Addr and View of the record to create AAAA Record
+// Optional fields: NetView, Ea, Cidr
+// Before creating, it checks if the Name and IP passed already exists in the network
+func (objMgr *ObjectManager) CreateAAAARecord(recA4 RecordAAAA) (*RecordAAAA, error) {
+
+	recA4.Ea = objMgr.extendEA(recA4.Ea)
+	recordA4 := NewRecordAAAA(recA4)
+
+	if recA4.Ipv6Addr == "" {
+		recordA4.Ipv6Addr = fmt.Sprintf("func:nextavailableip:%s,%s", recA4.Cidr, recA4.NetView)
+	} else {
+		recordA4.Ipv6Addr = recA4.Ipv6Addr
+	}
+	ref, err := objMgr.connector.CreateObject(recordA4)
+	recordA4.Ref = ref
+	return recordA4, err
+}
+
+// GetAAAARecord by passing Name, reference ID, IP Address or DNS View
+// If no arguments are passed then, all the records are returned
+func (objMgr *ObjectManager) GetAAAARecord(recA4 RecordAAAA) (*[]RecordAAAA, error) {
+		
+	var res []RecordAAAA
+	recordA4 := NewRecordAAAA(recA4)
+	var err error
+	if len(recA4.Ref)>0 {
+		err = objMgr.connector.GetObject(recordA4, recA4.Ref, &recordA4)
+		res = append(res,*recordA4)
+
+	} else {
+		recordA4 = NewRecordAAAA(recA4)
+		err = objMgr.connector.GetObject(recordA4, "", &res)
+		if err != nil || res == nil || len(res) == 0 {
+			return nil, err
+		}
+	}
+
+	return &res, err
+}
+
+// DeleteAAAARecord by passing either Reference or Name or IPv6Addr
+func (objMgr *ObjectManager) DeleteAAAARecord(recA4 RecordAAAA) (string, error) {
+	var res []RecordAAAA
+	recordName := NewRecordAAAA(recA4)
+	if len(recA4.Ref) > 0 {
+		return  objMgr.connector.DeleteObject(recA4.Ref)
+
+	} else {
+		recordName = NewRecordAAAA(recA4)
+		err := objMgr.connector.GetObject(recordName, "", &res)
+		if err != nil || res == nil || len(res) == 0 {
+			return "Record doesn't exist", err
+		}
+		return objMgr.connector.DeleteObject(res[0].Ref)
+	}
+}
+
+// UpdateAAAARecord takes Reference ID of the record as an argument
+// to update Name and EAs of the record
+func (objMgr *ObjectManager) UpdateAAAARecord(recA4 RecordAAAA) (*RecordAAAA, error) {
+	var res RecordAAAA
+	recordA4 := RecordAAAA{Name: recA4.Name}
+	recordA4.returnFields = []string {"name","extattrs"}
+	err := objMgr.connector.GetObject(&recordA4, recA4.Ref, &res)
+	if err != nil {
+		return nil, err
+	}
+	res.Name = recA4.Name
+
+	for k, v := range recA4.AddEA {
+		res.Ea[k] = v
+	}
+
+	for k := range recA4.RemoveEA {
+		_, ok := res.Ea[k]
+		if ok {
+			delete(res.Ea, k)
+		}
+	}
+	reference, err := objMgr.connector.UpdateObject(&res, recA4.Ref)
+	res.Ref= reference
+	err = objMgr.connector.GetObject(&recordA4, res.Ref, &res)
+	return &res, err
+}
---
+++ record_aaaa_test.go
@@ -0,0 +1,363 @@
+package ibclient
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Testing RecordAAAAOperations", func() {
+	Context("RecordAAAA object", func() {
+		ipv6addr := "fd60:e32:f1b9::4"
+		name := "bind_a.domain.com"
+		view := "default"
+		zone := "domain.com"
+
+		ra := NewRecordAAAA(RecordAAAA{
+			Ipv6Addr: ipv6addr,
+			Name:     name,
+			View:     view,
+			Zone:     zone})
+
+		It("should set fields correctly", func() {
+			Expect(ra.Ipv6Addr).To(Equal(ipv6addr))
+			Expect(ra.Name).To(Equal(name))
+			Expect(ra.View).To(Equal(view))
+			Expect(ra.Zone).To(Equal(zone))
+		})
+
+		It("should set base fields correctly", func() {
+			Expect(ra.ObjectType()).To(Equal("record:aaaa"))
+			Expect(ra.ReturnFields()).To(ConsistOf("ipv6addr", "name", "view", "zone","extattrs","comment","creation_time",
+				"creator","ddns_protected","dns_name","cloud_info","forbid_reclamation","last_queried",
+				"reclaimable","ttl","use_ttl","aws_rte53_record_info","ddns_principal","disable","discovered_data","ms_ad_user_data"))
+		})
+	})
+
+	Describe("Allocate specific AAAA Record ", func() {
+		vmID := "93f9249abc039284"
+		vmName := "dummyvm"
+		ea1 := EA{"VM ID": vmID, "VM Name": vmName}
+
+		recA4 := RecordAAAA{NetView: "private",
+			Cidr: "fd60:e32:f1b9::/32",
+			Ipv6Addr: "fd60:e32:f1b9::4",
+			View:     "default",
+			Name:     "test",
+			Ea:       ea1,
+			}
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", recA4.Name)
+		aniFakeConnector := &fakeConnector{
+			createObjectObj: NewRecordAAAA(recA4),
+			getObjectRef:    fakeRefReturn,
+			getObjectObj: NewRecordAAAA(RecordAAAA{
+				Name:     recA4.Name,
+				View:     recA4.View,
+				Ipv6Addr: recA4.Ipv6Addr,
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			resultObject:  NewRecordAAAA(RecordAAAA{
+				NetView: "private",
+				Cidr: "fd60:e32:f1b9::/32",
+				Ipv6Addr: "fd60:e32:f1b9::4",
+				View:     "default",
+				Name:     "test",
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			fakeRefReturn: fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		ea := objMgr.getBasicEA(true)
+		aniFakeConnector.createObjectObj.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM ID"] = vmID
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM Name"] = vmName
+		aniFakeConnector.getObjectObj.(*RecordAAAA).Ea = ea
+
+		var actualRecord *RecordAAAA
+		var err error
+		It("should pass expected AAAA record Object to CreateObject", func() {
+			actualRecord, err = objMgr.CreateAAAARecord(recA4)
+		})
+
+		It("should return expected AAAA record Object", func() {
+			Expect(actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Allocate AAAA Record in default network view when Cidr and network view is not passed", func() {
+		vmID := "93f9249abc039284"
+		vmName := "dummyvm"
+		ea1 := EA{"VM ID": vmID, "VM Name": vmName}
+
+		recA4 := RecordAAAA{
+			View:     "default",
+			Name:     "test",
+			Ipv6Addr: "fd60:e32:f1b9::4",
+			Ea:       ea1,
+		}
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", recA4.Name)
+		aniFakeConnector := &fakeConnector{
+			createObjectObj: NewRecordAAAA(recA4),
+			getObjectRef:    fakeRefReturn,
+			getObjectObj: NewRecordAAAA(RecordAAAA{
+				Name:     recA4.Name,
+				View:     recA4.View,
+				Ipv6Addr: "fd60:e32:f1b9::4",
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			resultObject:  NewRecordAAAA(RecordAAAA{
+				Ipv6Addr: "fd60:e32:f1b9::4",
+				View:     "default",
+				Name:     "test",
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			fakeRefReturn: fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		ea := objMgr.getBasicEA(true)
+		aniFakeConnector.createObjectObj.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM ID"] = vmID
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM Name"] = vmName
+		aniFakeConnector.getObjectObj.(*RecordAAAA).Ea = ea
+
+		var actualRecord *RecordAAAA
+		var err error
+		It("should pass expected AAAA record Object to CreateObject", func() {
+			actualRecord, err = objMgr.CreateAAAARecord(recA4)
+		})
+
+		It("should return expected AAAA record Object", func() {
+			Expect(actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Allocate next available AAAA Record ", func() {
+		vmID := "93f9249abc039284"
+		vmName := "dummyvm"
+		ea1 := EA{"VM ID": vmID, "VM Name": vmName}
+
+		recA4 := RecordAAAA{NetView: "private",
+			Cidr:     "fd60:e32:f1b9::/32",
+			View:     "default",
+			Name:     "test",
+			Ea:       ea1,
+		}
+		recA4.Ipv6Addr = fmt.Sprintf("func:nextavailableip:%s,%s", recA4.Cidr, recA4.NetView)
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", recA4.Name)
+		aniFakeConnector := &fakeConnector{
+			createObjectObj: NewRecordAAAA(recA4),
+			getObjectRef:    fakeRefReturn,
+			getObjectObj: NewRecordAAAA(RecordAAAA{
+				Name:     recA4.Name,
+				View:     recA4.View,
+				Ipv6Addr: fmt.Sprintf("func:nextavailableip:%s,%s", recA4.Cidr, recA4.NetView),
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			resultObject:  NewRecordAAAA(RecordAAAA{
+				NetView: "private",
+				Cidr:     "fd60:e32:f1b9::/32",
+				Ipv6Addr: fmt.Sprintf("func:nextavailableip:%s,%s", recA4.Cidr, recA4.NetView),
+				View:     "default",
+				Name:     "test",
+				Ref:      fakeRefReturn,
+				Ea:       ea1,
+			}),
+			fakeRefReturn: fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		ea := objMgr.getBasicEA(true)
+		aniFakeConnector.createObjectObj.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea = ea
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM ID"] = vmID
+		aniFakeConnector.resultObject.(*RecordAAAA).Ea["VM Name"] = vmName
+		aniFakeConnector.getObjectObj.(*RecordAAAA).Ea = ea
+
+		var actualRecord *RecordAAAA
+		var err error
+		It("should pass expected AAAA record Object to CreateObject", func() {
+			actualRecord, err = objMgr.CreateAAAARecord(recA4)
+		})
+
+		It("should return expected AAAA record Object", func() {
+			Expect(actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+
+	Describe("Get AAAA Record by IPv6Addr", func() {
+		recA4 := RecordAAAA{Ipv6Addr: "fd60:e32:f1b9::4"}
+		name := "test"
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", name)
+		aniFakeConnector := &fakeConnector{
+			getObjectObj: NewRecordAAAA(recA4),
+			getObjectRef: "",
+			resultObject: []RecordAAAA{*NewRecordAAAA(RecordAAAA{Name: recA4.Name, Ref: fakeRefReturn, Ipv6Addr: recA4.Ipv6Addr})},
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		var actualRecord *[]RecordAAAA
+		var err error
+		It("should pass expected AAAA record Object to GetObject", func() {
+			actualRecord, err = objMgr.GetAAAARecord(recA4)
+
+		})
+
+		It("should return expected AAAA record Object", func() {
+			Expect(*actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Get AAAA Record by Name", func() {
+		recA4 := RecordAAAA{Name: "test"}
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", recA4.Name)
+		aniFakeConnector := &fakeConnector{
+			getObjectObj: NewRecordAAAA(recA4),
+			getObjectRef: "",
+			resultObject: []RecordAAAA{*NewRecordAAAA(RecordAAAA{Name: recA4.Name, Ref: fakeRefReturn})},
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		var actualRecord *[]RecordAAAA
+		var err error
+		It("should pass expected A record Object to GetObject", func() {
+			actualRecord, err = objMgr.GetAAAARecord(recA4)
+
+		})
+
+		It("should return expected A record Object", func() {
+			Expect(*actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Get AAAA Record by Reference", func() {
+		name := "test"
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", name)
+		recA4 := RecordAAAA{Ref: fakeRefReturn}
+		aniFakeConnector := &fakeConnector{
+			getObjectObj: NewRecordAAAA(recA4),
+			getObjectRef: fakeRefReturn,
+			resultObject: []RecordAAAA{*NewRecordAAAA(RecordAAAA{Name: recA4.Name, Ref: fakeRefReturn})},
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+
+		var actualRecord *[]RecordAAAA
+		var err error
+		It("should pass expected A record Object to GetObject", func() {
+			actualRecord, err = objMgr.GetAAAARecord(recA4)
+
+		})
+
+		It("should return expected A record Object", func() {
+			Expect(*actualRecord).To(Equal(aniFakeConnector.resultObject))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Delete AAAA Record by Reference", func() {
+
+		name := "delete_test"
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", name)
+		recA4 := RecordAAAA{Ref: fakeRefReturn}
+		aniFakeConnector := &fakeConnector{
+			deleteObjectRef: recA4.Ref,
+			fakeRefReturn:   fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+		var actualRef string
+		var err error
+		It("should pass expected A record Ref to DeleteObject", func() {
+			actualRef, err = objMgr.DeleteAAAARecord(recA4)
+		})
+		It("should return expected A record Ref", func() {
+			Expect(actualRef).To(Equal(fakeRefReturn))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Delete AAAA Record by Name", func() {
+		recA4 := RecordAAAA{Name: "delete_test"}
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", recA4.Name)
+		aniFakeConnector := &fakeConnector{
+			getObjectObj: NewRecordAAAA(recA4),
+			getObjectRef: "",
+			resultObject: []RecordAAAA{*NewRecordAAAA(RecordAAAA{Name: recA4.Name, Ref: fakeRefReturn})},
+			deleteObjectRef: fakeRefReturn,
+			fakeRefReturn:   fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+		var actualRef string
+		var err error
+		It("should pass expected A record Ref to DeleteObject", func() {
+			actualRef, err = objMgr.DeleteAAAARecord(recA4)
+		})
+		It("should return expected A record Ref", func() {
+			Expect(actualRef).To(Equal(fakeRefReturn))
+			Expect(err).To(BeNil())
+		})
+	})
+
+	Describe("Delete AAAA Record by IPv6Addr", func() {
+		recA4 := RecordAAAA{Ipv6Addr: "fd60:e32:f1b9::4"}
+		name := "delete_test"
+		fakeRefReturn := fmt.Sprintf("record:aaaa/ZG5zLmJpbmRfY25h:%s/%20%20", name)
+		aniFakeConnector := &fakeConnector{
+			getObjectObj: NewRecordAAAA(recA4),
+			getObjectRef: "",
+			resultObject: []RecordAAAA{*NewRecordAAAA(RecordAAAA{Name: name, Ref: fakeRefReturn, Ipv6Addr: recA4.Ipv6Addr})},
+			deleteObjectRef: fakeRefReturn,
+			fakeRefReturn:   fakeRefReturn,
+		}
+
+		cmpType := "Docker"
+		tenantID := "01234567890abcdef01234567890abcdef"
+		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
+		var actualRef string
+		var err error
+		It("should pass expected A record Ref to DeleteObject", func() {
+			actualRef, err = objMgr.DeleteAAAARecord(recA4)
+		})
+		It("should return expected A record Ref", func() {
+			Expect(actualRef).To(Equal(fakeRefReturn))
+			Expect(err).To(BeNil())
+		})
+	})
+})
