f04d5014eecaaf2b25b5b36b1d5625de13adca10
Anagha K H <43703238+Anaghakh@users.noreply.github.com>
2020-05-20 10:27:06 +0530

    Update record_a.go

--- record_a.go
+++ record_a.go
@@ -11,18 +11,18 @@ type RecordAOperations interface {
 	DeleteARecord(recA RecordA) (string, error)
 	UpdateARecord(recA RecordA) (*RecordA, error)
 }
-type Dhcpmember struct {
+type DhcpMember struct {
 	Ipv4addr string    `json:"ipv4addr,omitempty"`
 	Ipv6addr string    `json:"ipv6addr,omitempty"`
 	Name     string    `json:"name,omitempty"`
 }
-type Cloud_info struct {
+type CloudInfo struct {
 	Authority_type      string       `json:"authority_type,omitempty"`
-	Delegated_member    Dhcpmember   `json:"delegated_member,omitempty"`
-	Delegated_root      string       `json:"delegated_root,omitempty"`
-	Delegated_scope     string       `json:"delegated_scope,omitempty"`
-	Mgmt_platform       string       `json:"msmt_platform,omitempty"`
-	Owned_by_adaptor    bool         `json:"owned_by_adaptor,omitempty"`
+	DelegatedMember    Dhcpmember   `json:"delegated_member,omitempty"`
+	DelegatedRoot      string       `json:"delegated_root,omitempty"`
+	DelegatedScope     string       `json:"delegated_scope,omitempty"`
+	MgmtPlatform        string       `json:"msmt_platform,omitempty"`
+	OwnedByAdaptor      bool         `json:"owned_by_adaptor,omitempty"`
 	Tenant              string       `json:"tenant,omitempty"`
 	Usage               string       `json:"usage,omitempty"`
 }
@@ -49,7 +49,7 @@ type RecordA struct {
 	Reclaimable      bool     `json:"reclaimable,omitempty"`
  	Ttl              uint     `json:"ttl,omitempty"`
 	Use_ttl          bool     `json:"use_ttl,omitempty"`
-	//CloudInfo       Cloud_info `json:"cloud_info,omitempty"`
+	//CloudInfo      CloudInfo `json:"cloud_info,omitempty"`
 }
 
 // NewRecordA creates a new A Record type with objectType and returnFields 
