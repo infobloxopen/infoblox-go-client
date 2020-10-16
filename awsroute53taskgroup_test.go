4012a2fff44d9f81500b13fc326602bc2b055881
Anagha K H <43703238+Anaghakh@users.noreply.github.com>
2020-10-16 09:02:02 +0530

    Update awsroute53taskgroup_test.go

--- awsroute53taskgroup_test.go
+++ awsroute53taskgroup_test.go
@@ -6,12 +6,12 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Testing AwsRoute53TaskGroupOperations", func() {
-	Context("AwsRoute53TaskGroup object", func() {
+var _ = Describe("Testing AWSRoute53TaskGroupOperations", func() {
+	Context("AWSRoute53TaskGroup object", func() {
 		name := "task3"
 		networkView := "test_netview"
 		gridMember :="test.localdomain"
-		ra := NewAwsRte53TaskGroup(AwsRte53TaskGroup{
+		ra := NewAWSRte53TaskGroup(AWSRte53TaskGroup{
 			Name: name,
 			NetworkView: networkView,
 			GridMember: gridMember,
@@ -30,11 +30,11 @@ var _ = Describe("Testing AwsRoute53TaskGroupOperations", func() {
 		})
 	})
 
-	Describe("Allocate specific Aws Route53 Task ", func() {
+	Describe("Allocate specific AWS Route53 Task ", func() {
 		cmpType := "Docker"
 		tenantID := "01234567890abcdef01234567890abcdef"
 
-		aws := AwsRte53TaskGroup{
+		aws := AWSRte53TaskGroup{
 			Name: "test_sync",
 			NetworkView: "test_netview",
 			GridMember: "test.localdomain",
@@ -42,8 +42,8 @@ var _ = Describe("Testing AwsRoute53TaskGroupOperations", func() {
 		}
 		fakeRefReturn := fmt.Sprintf("awsrte53taskgroup/ZG5zLmJpbmRfY25h:%s/%20%20", aws.Name)
 		aniFakeConnector := &fakeConnector{
-			createObjectObj: NewAwsRte53TaskGroup(aws),
-			resultObject: NewAwsRte53TaskGroup(AwsRte53TaskGroup{
+			createObjectObj: NewAWSRte53TaskGroup(aws),
+			resultObject: NewAWSRte53TaskGroup(AWSRte53TaskGroup{
 				Ref: fakeRefReturn,
 				NetworkView: aws.NetworkView,
 				Name: aws.Name,
@@ -56,50 +56,50 @@ var _ = Describe("Testing AwsRoute53TaskGroupOperations", func() {
 
 		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
 
-		var actualTask *AwsRte53TaskGroup
+		var actualTask *AWSRte53TaskGroup
 		var err error
-		It("should pass expected AwsRte53Group Task Object to CreateObject", func() {
-			actualTask, err = objMgr.CreateAwsRte53TaskGroup(aws)
+		It("should pass expected AWSRte53Group Task Object to CreateObject", func() {
+			actualTask, err = objMgr.CreateAWSRte53TaskGroup(aws)
 		})
 
-		It("should return expected AwsRte53Group Task Object", func() {
+		It("should return expected AWSRte53Group Task Object", func() {
 			Expect(actualTask).To(Equal(aniFakeConnector.resultObject))
 			Expect(err).To(BeNil())
 		})
 	})
 
-	Describe("Get AwsRte53Group Task by Reference", func() {
+	Describe("Get AWSRte53Group Task by Reference", func() {
 		name := "test"
 		fakeRefReturn := fmt.Sprintf("awsrte53taskgroup/ZG5zLmJpbmRfY25h:%s/%20%20", name)
-		aws := AwsRte53TaskGroup{Ref: fakeRefReturn}
+		aws := AWSRte53TaskGroup{Ref: fakeRefReturn}
 		aniFakeConnector := &fakeConnector{
-			getObjectObj: NewAwsRte53TaskGroup(aws),
+			getObjectObj: NewAWSRte53TaskGroup(aws),
 			getObjectRef: fakeRefReturn,
-			resultObject: []AwsRte53TaskGroup{*NewAwsRte53TaskGroup(AwsRte53TaskGroup{Name: aws.Name, Ref: fakeRefReturn})},
+			resultObject: []AWSRte53TaskGroup{*NewAWSRte53TaskGroup(AWSRte53TaskGroup{Name: aws.Name, Ref: fakeRefReturn})},
 		}
 
 		cmpType := "Docker"
 		tenantID := "01234567890abcdef01234567890abcdef"
 		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
 
-		var actualTask *[]AwsRte53TaskGroup
+		var actualTask *[]AWSRte53TaskGroup
 		var err error
-		It("should pass expected AwsRte53Group Task Object to GetObject", func() {
-			actualTask, err = objMgr.GetAwsRte53TaskGroup(aws)
+		It("should pass expected AWSRte53Group Task Object to GetObject", func() {
+			actualTask, err = objMgr.GetAWSRte53TaskGroup(aws)
 
 		})
 
-		It("should return expected AwsRte53Group Task Object", func() {
+		It("should return expected AWSRte53Group Task Object", func() {
 			Expect(*actualTask).To(Equal(aniFakeConnector.resultObject))
 			Expect(err).To(BeNil())
 		})
 	})
 
-	Describe("Delete AwsRte53Group Task by Reference", func() {
+	Describe("Delete AWSRte53Group Task by Reference", func() {
 
 		name := "delete_test"
 		fakeRefReturn := fmt.Sprintf("awsrte53taskgroup/ZG5zLmJpbmRfY25h:%s/%20%20", name)
-		aws := AwsRte53TaskGroup{Ref: fakeRefReturn}
+		aws := AWSRte53TaskGroup{Ref: fakeRefReturn}
 		aniFakeConnector := &fakeConnector{
 			deleteObjectRef: aws.Ref,
 			fakeRefReturn:   fakeRefReturn,
@@ -110,10 +110,10 @@ var _ = Describe("Testing AwsRoute53TaskGroupOperations", func() {
 		objMgr := NewObjectManager(aniFakeConnector, cmpType, tenantID)
 		var actualRef string
 		var err error
-		It("should pass expected AwsRte53Group Task Ref to DeleteObject", func() {
-			actualRef, err = objMgr.DeleteAwsRte53TaskGroup(aws)
+		It("should pass expected AWSRte53Group Task Ref to DeleteObject", func() {
+			actualRef, err = objMgr.DeleteAWSRte53TaskGroup(aws)
 		})
-		It("should return expected AwsRte53Group Task Ref", func() {
+		It("should return expected AWSRte53Group Task Ref", func() {
 			Expect(actualRef).To(Equal(fakeRefReturn))
 			Expect(err).To(BeNil())
 		})
