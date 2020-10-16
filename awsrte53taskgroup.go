fab0a47b8010ace33f5a910f900c87bb64a60ac9
Anagha K H <43703238+Anaghakh@users.noreply.github.com>
2020-10-16 09:01:58 +0530

    Update awsrte53taskgroup.go

--- awsrte53taskgroup.go
+++ awsrte53taskgroup.go
@@ -1,13 +1,13 @@
 package ibclient
 
-type AwsRte53TaskOperations interface {
-	CreateAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*AwsRte53TaskGroup, error)
-	GetAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*[]AwsRte53TaskGroup, error)
-	DeleteAwsRte53TaskGroup(aws AwsRte53TaskGroup) (string, error)
-	UpdateAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*AwsRte53TaskGroup, error)
+type AWSRte53TaskOperations interface {
+	CreateAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*AWSRte53TaskGroup, error)
+	GetAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*[]AWSRte53TaskGroup, error)
+	DeleteAWSRte53TaskGroup(aws AWSRte53TaskGroup) (string, error)
+	UpdateAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*AWSRte53TaskGroup, error)
 }
 
-type AwsUser struct {
+type AWSUser struct {
 	IBBase          `json:"-"`
 	AccountID       string `json:"account_id,omitempty"`
 	AccessKeyId     string `json:"access_key_id,omitempty"`
@@ -18,18 +18,18 @@ type AwsUser struct {
 	Status          string `json:"status,omitempty"`
 }
 
-// NewAwsUser creates a new AwsUser type with objectType and returnFields
-func NewAwsUser(aws AwsUser) *AwsUser {
+// NewAWSUser creates a new AWSUser type with objectType and returnFields
+func NewAWSUser(aws AWSUser) *AWSUser {
 	res := aws
 	res.objectType = "awsuser"
 	res.returnFields = []string{"account_id", "last_used", "name", "nios_user_name", "status"}
 	return &res
 }
 
-// GetAwsUser gets the required details of the existing user
-func (objMgr *ObjectManager) GetAwsUser(aws AwsUser) (*AwsUser, error) {
-	var res []AwsUser
-	awsRte53 := NewAwsUser(aws)
+// GetAWSUser gets the required details of the existing user
+func (objMgr *ObjectManager) GetAWSUser(aws AWSUser) (*AWSUser, error) {
+	var res []AWSUser
+	awsRte53 := NewAWSUser(aws)
 	var err error
 	err = objMgr.connector.GetObject(awsRte53, "", &res)
 	if err != nil || res == nil || len(res) == 0 {
@@ -38,25 +38,8 @@ func (objMgr *ObjectManager) GetAwsUser(aws AwsUser) (*AwsUser, error) {
 
 	return &res[0], err
 }
-/*type AwsRte53Task struct {
-	IBBase           `json:"-"`
-	AwsUser          string `json:"aws_user,omitempty"`
-	CredentialsType  string `json:"credentials_type,omitempty"`
-	Disabled         Bool   `json:"disabled,omitempty"`
-	Filter           string `json:"filter,omitempty"`
-	LastRun          int    `json:"last_run,omitempty"`
-	Name             string `json:"name,omitempty"`
-	ScheduleInterval uint   `json:"schedule_interval,omitempty"`
-	ScheduleUnits    string `json:"schedule_units,omitempty"`
-	State            string `json:"state,omitempty"`
-	StateMsg         string `json:"state_msg,omitempty"`
-	StatusTimestamp  int    `json:"status_timestamp,omitempty"`
-	SyncPrivateZones Bool   `json:"sync_private_zones,omitempty"`
-	SyncPublicZones  Bool   `json:"sync_public_zones,omitempty"`
-	ZoneCount        uint   `json:"zone_count,omitempty"`
-}*/
 
-type AwsRte53TaskGroup struct {
+type AWSRte53TaskGroup struct {
 	IBBase                   `json:"-"`
 	Ref                      string `json:"_ref,omitempty"`
 	AccountId                string `json:"account_id,omitempty"`
@@ -71,8 +54,8 @@ type AwsRte53TaskGroup struct {
 	SyncStatus               string `json:"sync_status,omitempty"`
 }
 
-// NewAwsRte53TaskGroup creates a new AwsRte53TaskGroup type with objectType and returnFields
-func NewAwsRte53TaskGroup(aws AwsRte53TaskGroup) *AwsRte53TaskGroup {
+// NewAWSRte53TaskGroup creates a new AWSRte53TaskGroup type with objectType and returnFields
+func NewAWSRte53TaskGroup(aws AWSRte53TaskGroup) *AWSRte53TaskGroup {
 	res := aws
 	res.objectType = "awsrte53taskgroup"
 
@@ -81,31 +64,31 @@ func NewAwsRte53TaskGroup(aws AwsRte53TaskGroup) *AwsRte53TaskGroup {
 	return &res
 }
 
-// CreateAwsRte53Task takes Name, View, GridMember as arguments to create AwsRte53Task
-func (objMgr *ObjectManager) CreateAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*AwsRte53TaskGroup, error) {
+// CreateAWSRte53Task takes Name, View, GridMember as arguments to create AWSRte53Task
+func (objMgr *ObjectManager) CreateAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*AWSRte53TaskGroup, error) {
 
 	if aws.NetworkView != "" {
 		aws.NetworkViewMappingPolicy = "DIRECT"
 	}
-	awsRte53 := NewAwsRte53TaskGroup(aws)
+	awsRte53 := NewAWSRte53TaskGroup(aws)
 	ref, err := objMgr.connector.CreateObject(awsRte53)
 	awsRte53.Ref = ref
 	return awsRte53, err
 }
 
-// GetAwsRte53TaskGroup by passing Name, reference ID or NetworkView
+// GetAWSRte53TaskGroup by passing Name, reference ID or NetworkView
 // If no arguments are passed then, all the tasks are returned
-func (objMgr *ObjectManager) GetAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*[]AwsRte53TaskGroup, error) {
+func (objMgr *ObjectManager) GetAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*[]AWSRte53TaskGroup, error) {
 
-	var res []AwsRte53TaskGroup
-	awsRte53 := NewAwsRte53TaskGroup(aws)
+	var res []AWSRte53TaskGroup
+	awsRte53 := NewAWSRte53TaskGroup(aws)
 	var err error
 	if len(aws.Ref) > 0 {
 		err = objMgr.connector.GetObject(awsRte53, aws.Ref, &awsRte53)
 		res = append(res, *awsRte53)
 
 	} else {
-		awsRte53 = NewAwsRte53TaskGroup(aws)
+		awsRte53 = NewAWSRte53TaskGroup(aws)
 		err = objMgr.connector.GetObject(awsRte53, "", &res)
 		if err != nil || res == nil || len(res) == 0 {
 			return nil, err
@@ -115,15 +98,15 @@ func (objMgr *ObjectManager) GetAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*[]Aws
 	return &res, err
 }
 
-// DeleteAwsRte53TaskGroup by passing either Reference or Name
-func (objMgr *ObjectManager) DeleteAwsRte53TaskGroup(aws AwsRte53TaskGroup) (string, error) {
-	var res []AwsRte53TaskGroup
-	awsRte53 := NewAwsRte53TaskGroup(aws)
+// DeleteAWSRte53TaskGroup by passing either Reference or Name
+func (objMgr *ObjectManager) DeleteAWSRte53TaskGroup(aws AWSRte53TaskGroup) (string, error) {
+	var res []AWSRte53TaskGroup
+	awsRte53 := NewAWSRte53TaskGroup(aws)
 	if len(aws.Ref) > 0 {
 		return objMgr.connector.DeleteObject(aws.Ref)
 
 	} else {
-		awsRte53 = NewAwsRte53TaskGroup(aws)
+		awsRte53 = NewAWSRte53TaskGroup(aws)
 		err := objMgr.connector.GetObject(awsRte53, "", &res)
 		if err != nil || res == nil || len(res) == 0 {
 			return "Task doesn't exist", err
@@ -133,12 +116,12 @@ func (objMgr *ObjectManager) DeleteAwsRte53TaskGroup(aws AwsRte53TaskGroup) (str
 
 }
 
-// UpdateAwsRte53GroupTask takes Reference ID of the task as an argument
+// UpdateAWSRte53GroupTask takes Reference ID of the task as an argument
 // to update Name
 // returns updated Refernce ID
-func (objMgr *ObjectManager) UpdateAwsRte53TaskGroup(aws AwsRte53TaskGroup) (*AwsRte53TaskGroup, error) {
-	var res AwsRte53TaskGroup
-	awsRte53 := AwsRte53TaskGroup{}
+func (objMgr *ObjectManager) UpdateAWSRte53TaskGroup(aws AWSRte53TaskGroup) (*AWSRte53TaskGroup, error) {
+	var res AWSRte53TaskGroup
+	awsRte53 := AWSRte53TaskGroup{}
 	awsRte53.returnFields = []string{"name"}
 	err := objMgr.connector.GetObject(&awsRte53, aws.Ref, &res)
 	if err != nil {
