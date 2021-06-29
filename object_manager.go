22d237f611e2fbfc03381df4f6d2d7a792ab0852
survivorbat <djbatcat@gmail.com>
2021-06-29 10:14:31 +0200

    Add compile-time interface check

--- object_manager.go
+++ object_manager.go
@@ -9,6 +9,9 @@ import (
 	"regexp"
 )
 
+// Compile-time interface checks
+var _ IBObjectManager = new(ObjectManager)
+
 type IBObjectManager interface {
 	AllocateIP(netview string, cidr string, ipAddr string, isIPv6 bool, macAddress string, name string, comment string, eas EA) (*FixedAddress, error)
 	AllocateNetwork(netview string, cidr string, isIPv6 bool, prefixLen uint, comment string, eas EA) (network *Network, err error)
