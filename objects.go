d08e24bc66371e12e726f3e31f0ff29d93bed493
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2022-12-21 14:39:18 -0500

    Applied the merge changes to the objects.go and object_manager.go files

--- objects.go
+++ objects.go
@@ -292,6 +292,12 @@ func NewFixedAddress(
 	return res
 }
 
+func NewEmptyDNSView() *View {
+	res := &View{}
+	res.returnFields = []string{"extattrs", "name", "network_view", "comment"}
+	return res
+}
+
 type Network struct {
 	IBBase      `json:"-"`
 	objectType  string
@@ -317,7 +323,7 @@ func NewNetwork(netview string, cidr string, isIPv6 bool, comment string, ea EA)
 	} else {
 		res.objectType = "network"
 	}
-	res.returnFields = []string{"extattrs", "network", "comment"}
+	res.returnFields = []string{"extattrs", "network", "network_view", "comment"}
 
 	return &res
 }
